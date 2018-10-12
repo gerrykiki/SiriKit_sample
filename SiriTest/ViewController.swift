9e7047027fc561d000f87ed2165b745994a8952e
Gerry PW Lin <gerry_pw_lin@wistron.com>
2018-10-12 10:37:19 +0800

    Siri kit commit

--- SiriTest/ViewController.swift
+++ SiriTest/ViewController.swift
@@ -7,17 +7,21 @@
 //
 
 import UIKit
+import Intents
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        INPreferences.requestSiriAuthorization { (status) in
+            switch status {
+            case .authorized:
+                print("Autorized")
+            default:
+                print("Not Autorized")
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
